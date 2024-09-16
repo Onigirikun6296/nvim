67b755702b6e1b275fda595a02d488bfd760ba97
Oni <oni@onigiribentou.com>
2024-09-16 20:29:05 +0300

    Update nabla config

--- lua/plugins/nabla.lua
+++ lua/plugins/nabla.lua
@@ -1,7 +1,10 @@
 return {
   'jbyuki/nabla.nvim',
   config = function()
-    vim.keymap.set("n", "<leader>v", require("nabla").enable_virt, {noremap = true})
+    vim.keymap.set("n", "<leader>v", function() 
+      require("nabla").enable_virt()
+      vim.opt.concealcursor = 'nc'
+	end, {noremap = true})
     vim.keymap.set("n", "<leader>p", function()
       require("nabla").popup()
       -- Customize with popup({border = ...})  : `single` (default), `double`, `rounded`
