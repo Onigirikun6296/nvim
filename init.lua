1bf9227437d5d778890bbe7ab4983250c0052a7c
Oni <oni@onigiribentou.com>
2024-09-16 20:29:26 +0300

    Set expandtab to true

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.opt.clipboard      = "unnamedplus"
 vim.opt.tabstop     = 4
 vim.opt.shiftwidth  = 4
 vim.opt.softtabstop = 4
+vim.opt.expandtab   = true
 vim.opt.autoindent  = true
 vim.opt.smartindent = true
 
